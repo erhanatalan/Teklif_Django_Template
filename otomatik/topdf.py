8faf0a2ccbe84926abc6093bf5dfbf66246ae158
erhanatalan <erhanatalan2013@gmail.com>
2023-07-22 19:24:29 +0300

    topdf

--- otomatik/topdf.py
+++ otomatik/topdf.py
@@ -1,13 +1,24 @@
+import subprocess
 
-def wordtopdf(docxPathName,pdfPathName ):
-    from docx2pdf import convert
-    import time
-    # Word belgesini PDF'ye dönüştürmek için dosya yolunu belirtin
-    docx_path = docxPathName
-    pdf_path = pdfPathName
+def wordtopdf(docxPathName, pdfPathName):
+    try:
+        # LibreOffice komutunu çalıştırarak DOCX dosyasını PDF'ye dönüştürün
+        subprocess.run(['libreoffice', '--headless', '--convert-to', 'pdf', docxPathName])
 
-    # Word belgesini PDF'ye dönüştürün
-    convert(docx_path, pdf_path)
+        print(f"{pdfPathName} dosyası oluşturuldu.")
+    except Exception as e:
+        print(f"Dönüştürme hatası: {e}")
 
-    print(f"{pdf_path} dosyası oluşturuldu.")
-    time.sleep(2)
+
+# def wordtopdf(docxPathName,pdfPathName ):
+#     from docx2pdf import convert
+#     import time
+#     # Word belgesini PDF'ye dönüştürmek için dosya yolunu belirtin
+#     docx_path = docxPathName
+#     pdf_path = pdfPathName
+
+#     # Word belgesini PDF'ye dönüştürün
+#     convert(docx_path, pdf_path)
+
+#     print(f"{pdf_path} dosyası oluşturuldu.")
+#     time.sleep(2)
