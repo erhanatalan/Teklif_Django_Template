bc94a9a555969b1d8fac24093fa7aa2f0cc425d3
erhanatalan <erhanatalan2013@gmail.com>
2023-07-22 18:10:22 +0300

    settings dil

--- main/settings.py
+++ main/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr'  # veya dil kodunuza göre başka bir değer
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Istanbul'  # veya bölgenize göre başka bir değer
 
 USE_I18N = True
 
