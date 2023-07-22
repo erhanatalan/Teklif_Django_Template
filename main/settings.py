95854931c2251644562ab47ba29a3900a81035fd
erhanatalan <erhanatalan2013@gmail.com>
2023-07-22 20:28:52 +0300

    false

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3)-(t8iomh(=erk&gg2)d^91wzj)#cs%^u6yfl$)g8iuoscnyp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
