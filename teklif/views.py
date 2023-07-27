1356148adfc8f7cb3dee7566a20fc643da3ebe60
erhanatalan <erhanatalan2013@gmail.com>
2023-07-27 13:32:26 +0300

    transaction.commit()

--- teklif/views.py
+++ teklif/views.py
@@ -1,35 +1,28 @@
 from django.shortcuts import render, redirect
 from .forms import TeklifForm
-# from django.views.decorators.csrf import csrf_protect
 import time
 import os
-import fitz
-from PIL import Image
 from django.conf import settings
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
+from django.db import transaction
 
 pdf_dizini = os.path.join(settings.BASE_DIR, 'otomatik/pdf')
 
 def offer_success_view(request):
     return render(request, 'offer_success.html')
 
-@csrf_exempt
 def teklif_submit_view(request):
     if request.method == 'POST':
         form = TeklifForm(request.POST)
         if form.is_valid():
+            form = TeklifForm()
             form.save()
-            if form.save():
-                form = TeklifForm()
-                from otomatik.run import run
-                run()
-                # pdf_preview()
-                return redirect('offer_success_view')  # Eğer teklif başarılı bir şekilde kaydedildiyse başka bir sayfaya yönlendirilebilirsiniz.
-            # return redirect(request, 'home.html', {'form': form})
+            transaction.commit()
+            from otomatik.run import run
+            run()
+            # pdf_preview()
+            return redirect('offer_success_view')
     else:
         form = TeklifForm(initial={'uzunluk': 16, 'tonaj': 80, 'indikator':'ABS-B3', 'usmodel':'B', 'yazar':'Erhan ATALAN'})
-    #return redirect('home_view')  # Eğer teklif başarılı bir şekilde kaydedildiyse başka bir sayfaya yönlendirilebilirsiniz.
     return render(request, 'home.html', {'form': form})
 
 
