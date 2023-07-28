80ad1cea9de25fc66817ee51a2f603102dceb350
erhanatalan <erhanatalan2013@gmail.com>
2023-07-28 17:08:19 +0300

    save

--- teklif/views.py
+++ teklif/views.py
@@ -10,11 +10,11 @@ pdf_dizini = os.path.join(settings.BASE_DIR, 'otomatik/pdf')
 def offer_success_view(request):
     return render(request, 'offer_success.html')
 
-def teklif_submit_view(request):
+async def teklif_submit_view(request):
     if request.method == 'POST':
         form = TeklifForm(request.POST)
         if form.is_valid():
-            form.save()
+            await form.save()
             # latest_teklif = Teklif.objects.latest('id')
             from otomatik.run import run
             run()
