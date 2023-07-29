fcea38c3761c2a0ca3e7e3cd5536f9351c3a8fb0
erhanatalan <erhanatalan2013@gmail.com>
2023-07-29 13:55:22 +0300

    try

--- teklif/views.py
+++ teklif/views.py
@@ -11,22 +11,27 @@ def offer_success_view(request):
     return render(request, 'offer_success.html')
 
 def teklif_submit_view(request):
-    if request.method == 'POST':
-        form = TeklifForm(request.POST)
-        if form.is_valid():
-            print('save basladi')
-            s1 = form.save()
-            field1_value = s1.field1  # Replace 'field1' with the actual field name
-            field2_value = s1.field2
-            print(f'Field1: {field1_value}, Field2: {field2_value}')
-            # latest_teklif = Teklif.objects.latest('id')
-            if s1 :
-                from otomatik.run import run
-                run()
-                return redirect('offer_success_view'), s1
-    else:
-        form = TeklifForm(initial={'uzunluk': 16, 'tonaj': 80, 'indikator':'ABS-B3', 'usmodel':'B', 'yazar':'Erhan ATALAN', 'cekvade':60, 'vinc':'ALICI FIRMA TARAFINDAN','insaat':'ALICI FIRMA TARAFINDAN','nakliye':'ALICI FIRMA TARAFINDAN'})
-    return render(request, 'home.html', {'form': form})
+
+    try:
+        if request.method == 'POST':
+            form = TeklifForm(request.POST)
+            if form.is_valid():
+                print('save basladi')
+                s1 = form.save()
+                field1_value = s1.field1  # Replace 'field1' with the actual field name
+                field2_value = s1.field2
+                print(f'Field1: {field1_value}, Field2: {field2_value}')
+                # latest_teklif = Teklif.objects.latest('id')
+                if s1 :
+                    from otomatik.run import run
+                    run()
+                    return redirect('offer_success_view'), s1
+        else:
+            form = TeklifForm(initial={'uzunluk': 16, 'tonaj': 80, 'indikator':'ABS-B3', 'usmodel':'B', 'yazar':'Erhan ATALAN', 'cekvade':60, 'vinc':'ALICI FIRMA TARAFINDAN','insaat':'ALICI FIRMA TARAFINDAN','nakliye':'ALICI FIRMA TARAFINDAN'})
+        return render(request, 'home.html', {'form': form})
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
 
 
 # def pdf_preview(request):
